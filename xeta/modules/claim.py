a891033892e32219b71f473a45d862a0838d3caa
Synth <41410706+synthXR@users.noreply.github.com>
2021-11-22 16:01:31 +0000

    feat: wrapper claim methods

--- xeta/modules/claim.py
+++ xeta/modules/claim.py
@@ -1,20 +1,18 @@
-from xeta.modules import transaction
-from xeta.library import models, utils
+from xeta.modules import instruction, resource
+from xeta.library import models, utils, hashed
 from xeta.library.config import config
-import json
-import time
 
 
-def create(owner, token, tokenAmount, xetaAmount=None, expires=None, unlocks=None, frozen=None, category=None, meta=None, answer=None, number=None, raw=False):
+def create(owner, token, tokenAmount, xetaAmount=None, expires=None, unlocks=None, frozen=None, category=None, meta=None, answer=None, number=None, tx={}):
     """
     Create claim
     """
-    instruction = utils.strip({
+    return instruction.wrap({
         'function': 'claim.create',
         'owner': owner,
         'token': token,
-        'tokenAmount': tokenAmount,
-        'xetaAmount': xetaAmount,
+        'tokenAmount': utils.amount(tokenAmount),
+        'xetaAmount': utils.amount(xetaAmount),
         'expires': expires,
         'unlocks': unlocks,
         'frozen': frozen,
@@ -22,19 +20,17 @@ def create(owner, token, tokenAmount, xetaAmount=None, expires=None, unlocks=Non
         'meta': meta,
         'answer': answer,
         'number': number,
-    })
+    }, tx)
 
-    if raw: return instruction
-    return transaction.create({'instructions': [instruction]})
-
-def update(tokenAmount=None, xetaAmount=None, expires=None, unlocks=None, frozen=None, category=None, meta=None, answer=None, number=None, raw=False):
+def update(claim, tokenAmount=None, xetaAmount=None, expires=None, unlocks=None, frozen=None, category=None, meta=None, answer=None, number=None, tx={}):
     """
     Update claim
     """
-    instruction = utils.strip({
+    return instruction.wrap({
         'function': 'claim.update',
-        'tokenAmount': tokenAmount,
-        'xetaAmount': xetaAmount,
+        'claim': claim,
+        'tokenAmount': utils.amount(tokenAmount),
+        'xetaAmount': utils.amount(xetaAmount),
         'expires': expires,
         'unlocks': unlocks,
         'frozen': frozen,
@@ -42,110 +38,197 @@ def update(tokenAmount=None, xetaAmount=None, expires=None, unlocks=None, frozen
         'meta': meta,
         'answer': answer,
         'number': number,
-    })
+    }, tx)
 
-    if raw: return instruction
-    return transaction.create({'instructions': [instruction]})
-
-def transfer(claim, raw=False):
+def transfer(claim, to, tx={}):
     """
     Transfer claim
     """
-    instruction = utils.strip({
+    return instruction.wrap({
         'function': 'claim.transfer',
         'claim': claim,
         'to': to,
-    })
+    }, tx)
 
-    if raw: return instruction
-    return transaction.create({'instructions': [instruction]})
-
-def resolve(claim, raw=False):
+def resolve(claim, tx={}):
     """
     Resolve claim
     """
-    instruction = utils.strip({
+    return instruction.wrap({
         'function': 'claim.resolve',
         'claim': claim,
-    })
+    }, tx)
 
-    if raw: return instruction
-    return transaction.create({'instructions': [instruction]})
-
-
-def scanByClaim(creator, owner, token, address=None, created=None, sort='DESC', limit=25):
+def readHash(hash, args={}):
     """
-    Scan tokens by claim
+    Read claim by hash
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'creator': creator, 'owner': owner, 'token': token, 'address': address, 'created': created, 'sort': sort, 'limit': limit}))
+    return resource.read(**{**{
+        'type': 'claim',
+        'key': hash,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByHolder(holder, address=None, created=None, sort='DESC', limit=25):
+def listHashes(hashes, args={}):
     """
-    Scan tokens by holder
+    List claims by hashes
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'holder': holder, 'address': address, 'created': created, 'sort': sort, 'limit': limit}))
+    return resource.list(**{**{
+        'type': 'claim',
+        'keys': hashes,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByIssuer(cluster, address=None, created=None, sort='DESC', limit=25):
+def scanHolderCategoryCreated(holder, category, created=None, hash=None, args={}):
     """
-    Scan tokens by issuer
+    Scan claims by holder and category, sort by created
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'issuer': issuer, 'address': address, 'created': created, 'sort': sort, 'limit': limit}))
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'holderCategory,
+        'indexValue': hashed.values([holder, category])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByIssuerAmount(issuer, address=None, amount=None, sort='DESC', limit=25):
+def scanIssuerCategoryCreated(issuer, category, created=None, hash=None, args={}):
     """
-    Scan tokens by issuer sorted by amount
+    Scan claims by issuer and category, sort by created
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'issuer': issuer, 'address': address, 'amount': amount, 'sort': sort, 'limit': limit}))
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuerCategory,
+        'indexValue': hashed.values([issuer, category])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByIssuerRandom(issuer, address=None, random=None, sort='DESC', limit=25):
+def scanIssuerAnswer(issuer, answer=None, hash=None, args={}):
     """
-    Scan tokens by issuer sorted by random
+    Scan claims by issuer, sort by answer
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'issuer': issuer, 'address': address, 'random': random, 'sort': sort, 'limit': limit}))
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuer,
+        'indexValue': issuer,
+        'sort': 'answer',
+        'sortValue': answer,
+        'keyValue': token,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByIssuerAnswer(issuer, address=None, answer=None, sort='DESC', limit=25):
+def scanIssuerNumber(issuer, number=None, hash=None, args={}):
     """
-    Scan tokens by issuer and answer
+    Scan claims by issuer, sort by number
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'issuer': issuer, 'address': address, 'answer': answer, 'sort': sort, 'limit': limit}))
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuer,
+        'indexValue': issuer,
+        'sort': 'number',
+        'sortValue': number,
+        'keyValue': token,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
-
-def scanByIssuerNumber(issuer, address=None, number=None, sort='DESC', limit=25):
+def scanIssuerTokenAmount(issuer, tokenAmount=None, hash=None, args={}):
     """
-    Scan tokens by issuer and number
+    Scan claims by issuer, sort by tokenAmount
     """
-    results = utils.request(
-        method='GET',
-        url=config['interface']+'/tokens',
-        params=utils.strip({'issuer': issuer, 'address': address, 'number': number, 'sort': sort, 'limit': limit}))
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuer,
+        'indexValue': issuer,
+        'sort': 'tokenAmount',
+        'sortValue': tokenAmount,
+        'keyValue': token,
+    }, **args})
 
-    return [models.parse_values(r, models.TOKEN) for r in results]
+def scanIssuerXetaAmount(issuer, xetaAmount=None, hash=None, args={}):
+    """
+    Scan claims by issuer, sort by xetaAmount
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuer,
+        'indexValue': issuer,
+        'sort': 'xetaAmount',
+        'sortValue': xetaAmount,
+        'keyValue': token,
+    }, **args})
+
+def scanIssuerCreated(issuer, created=None, hash=None, args={}):
+    """
+    Scan claims by issuer, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuer,
+        'indexValue': hashed.values([issuer])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
+
+def scanHolderCreated(holder, created=None, hash=None, args={}):
+    """
+    Scan claims by holder, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'holder,
+        'indexValue': hashed.values([holder])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
+
+def scanIssuerTokenCreated(issuer, token, created=None, hash=None, args={}):
+    """
+    Scan claims by issuer and token, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuerToken,
+        'indexValue': hashed.values([issuer, token])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
+
+def scanHolderTokenCreated(holder, token, created=None, hash=None, args={}):
+    """
+    Scan claims by holder and token, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'holderToken,
+        'indexValue': hashed.values([holder, token])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
+
+def scanIssuerHolder(issuer, holder, created=None, hash=None, args={}):
+    """
+    Scan claims by issuer and holder, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuerHolder,
+        'indexValue': hashed.values([issuer, holder])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
+
+def scanIssuerHolderToken(issuer, holder, token, created=None, hash=None, args={}):
+    """
+    Scan claims by issuer, holder and token, sort by created
+    """
+    return resource.scan(**{**{
+        'type': 'claim',
+        'index': 'issuerHolderToken,
+        'indexValue': hashed.values([issuer, holder, token])[-8:],
+        'sort': 'created',
+        'sortValue': created,
+        'keyValue': token,
+    }, **args})
